965c3a1a633e433a3110803a5bc7304909fc4864
MABDC-admin <admin@mabdc.ae>
2026-01-27 23:56:22 +0400

    Set up Capacitor and GitHub Actions for automated APK build

---
+++ android/app/src/main/java/com/mabdc/galanight/MainActivity.java
@@ -0,0 +1,5 @@
+package com.mabdc.galanight;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withPWA = require('next-pwa')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  turbopack: {}
+  turbopack: {},
 }
 
 module.exports = withPWA(nextConfig)
